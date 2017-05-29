55e173d0a54c89c28427d4521474f1d3ea77359c
Thiago Perrotta <tbperrotta@gmail.com>
2017-05-28 23:35:51 -0300

    add select

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import select
 import socket
 import sys
 
@@ -8,21 +9,35 @@ class ChatServer:
     def __init__(self,
                  hostname='',
                  port=8000,
+                 logger=logging.getLogger('server')
                  ):
-        self.logger = logging.getLogger('server')
-
         self.hostname = hostname
         self.port = port
+        self.logger = logger
+
+        self.inputs = []
+        self.outputs = []
 
     def start(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(0)
 
-        self.logger.info('Starting on %s:%d', self.hostname, self.port)
+        self.logger.info('starting on %s:%d', self.hostname, self.port)
         self.socket.bind((self.hostname, self.port))
 
         self.socket.listen(5)
 
-        logging.getLogger('server').info('Waiting to accept client connection...')
+        self.inputs.append(self.socket)
+
+        while self.inputs:
+            self.logger.info('waiting for next event')
+            rlist, wlist, xlist = select.select(self.inputs,
+                                                self.outputs,
+                                                self.inputs)
+
+
+
+        # self.logger.info('waiting to accept client connection')
         # (client, address) = self.socket.accept()
 
         return 0
@@ -31,9 +46,10 @@ class ChatServer:
 if __name__ == '__main__':
     logging.basicConfig(filename='pychat.log',
                         filemode='w',
-                        format='%(levelname)s:%(asctime)s:%(name)s:%(module)s.%(funcName)s:%(message)s',
                         level=logging.INFO
                         )
+
+    # TODO(tperrotta): argument parsing
 
     server = ChatServer()
     sys.exit(server.start())
